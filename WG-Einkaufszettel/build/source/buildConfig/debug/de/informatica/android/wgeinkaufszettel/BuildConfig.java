c8484ca592d1db4de344d8cbdefc54bc7cccc5d6
Informatika <elka.xharo@gmail.com>
2013-09-03 14:25:35 +0200

    Initial commit

---
+++ WG-Einkaufszettel/build/source/buildConfig/debug/de/informatica/android/wgeinkaufszettel/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.informatica.android.wgeinkaufszettel;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
